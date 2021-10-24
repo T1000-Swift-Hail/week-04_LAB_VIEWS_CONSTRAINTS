e7a877666035f2579ec73fa99f25713cdfbcdca6
azoz <aabdoalaziz1@gmail.com>
2021-10-24 15:23:14 +0300

    My First App is Done

---
+++ My First iOS App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My First iOS App
+//
+//  Created by Mac on 24/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var LBF: UILabel!
+    
+}
+
---
+++ My First iOS AppUITests/My_First_iOS_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  My_First_iOS_AppUITestsLaunchTests.swift
+//  My First iOS AppUITests
+//
+//  Created by Mac on 24/10/2021.
+//
+
+import XCTest
+
+class My_First_iOS_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
