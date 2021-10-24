d69f49e61303eb6a9ccd31b152d912be510e92bb
mac <mac@MacBook-Pro-alkhas-b-mac.local>
2021-10-24 14:35:02 +0300

    Dine

---
+++ VIEWS_CONSTRAINTS/VIEWS_CONSTRAINTS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIEWS_CONSTRAINTS
+//
+//  Created by mac on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
