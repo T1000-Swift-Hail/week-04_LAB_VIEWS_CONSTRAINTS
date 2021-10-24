e679f1753d5dc9c0029d22a5a5f764c5947df921
MACBOOK <macbook@Az-MacBook-Pro.local>
2021-10-24 13:37:30 +0300

    My1st app it's Done

---
+++ MyFirstiosApp/MyFirstiosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstiosApp
+//
+//  Created by MACBOOK on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
