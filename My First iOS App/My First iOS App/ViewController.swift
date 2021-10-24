f10ada491ba9b72838ae098f93c4c7d10e6c1482
Monafh <monafh@MacBook-Pro-alkhas-b-Monafh.local>
2021-10-24 12:13:38 +0300

    OK Done

---
+++ My First iOS App/My First iOS App/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  My First iOS App
+//
+//  Created by Monafh on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var Hailcomp: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
