8b3ac2bc927d70a3c7dbb266936ee38fdc924b20
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-10-24 13:50:00 +0300

    done lab

---
+++ My First iOS App/My First iOS App/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  My First iOS App
+//
+//  Created by HIND12 on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var text: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
