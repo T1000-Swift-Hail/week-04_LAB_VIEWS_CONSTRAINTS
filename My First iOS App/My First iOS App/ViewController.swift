36d9e8440e0935565cab00a1dfa076c225c8227d
dalal92 <dalal@MacBook-Pro.local>
2021-10-24 12:14:56 +0300

    Done

---
+++ My First iOS App/My First iOS App/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  My First iOS App
+//
+//  Created by Dalal AlSaidi on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
