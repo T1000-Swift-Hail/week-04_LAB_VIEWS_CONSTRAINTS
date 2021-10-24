59616c76b7dea91fa2d13626a034ffbd55be226f
Majed Alshammari <majed.wanis.has.s@tuwaiq.edu.sa>
2021-10-24 13:42:38 +0300

    done

---
+++ MyFirst iOS App/MyFirst iOS App/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MyFirst iOS App
+//
+//  Created by Majed Alshammari on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+//    @IBOutlet weak var Lablel: UILabel!
+//
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+//        Lablel.text = "Hail Swift Camp"
+    }
+
+
+}
+
