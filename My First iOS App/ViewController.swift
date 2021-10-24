b2069bfff8c888ee9a0d72a1ebb37de20cd12145
موضي الحربي <modhialhrbi@MacBook-Air-mwdy.local>
2021-10-24 15:29:59 +0300

    updated

---
+++ My First iOS App/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  My First iOS App
+//
+//  Created by موضي الحربي on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   @IBOutlet weak var String : UILabel!
+    
+                            
+                            
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+            string.text = "Hail Swift Camp"
+        
+        }
+}
+
