780ba6b6199fc4fc85330d958a3be1086c8cab70
Aryaf AlQabali <Aryaf.Aqeel.has.s@tuwaiq.edu.sa>
2021-10-24 13:03:46 +0300

    done  My First iOS App

---
+++ My First iOS App/My First iOS App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My First iOS App
+//
+//  Created by Arr Alq on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
