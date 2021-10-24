b86ac9b60cfe80f05e8dbdab986cb8ea913c45bf
Seham الشطنان <sehamalshtnan@Sehams-MacBook-Air.local>
2021-10-24 13:15:02 +0300

    done

---
+++ My First iOS App/My First iOS App/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.sw-ift
+//  My First iOS App
+//
+//  Created by Seham الشطنان on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var teast: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
