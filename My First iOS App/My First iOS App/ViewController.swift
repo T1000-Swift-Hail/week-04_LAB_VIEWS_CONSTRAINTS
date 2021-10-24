8ce425d961411925e011d1a829ac2b72880de80b
ahmad1aa <Ahmed.Atallah.has.s@tuwaiq.edu.sa>
2021-10-24 13:34:31 +0300

    done my first app

---
+++ My First iOS App/My First iOS App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My First iOS App
+//
+//  Created by Ahmed Alenazi on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
