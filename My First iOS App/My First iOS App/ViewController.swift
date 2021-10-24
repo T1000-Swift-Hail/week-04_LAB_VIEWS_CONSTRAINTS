506001b06970dcfdd30bc028a7c4834abe14da34
iAbdullah17 <user@MacBook-Air-.local>
2021-10-24 13:17:35 +0300

    First Test App is Done

---
+++ My First iOS App/My First iOS App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My First iOS App
+//
+//  Created by iAbdullah17 on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+override func viewDidLoad() {
+        super.viewDidLoad()
+        // D;ooverride  any additional setup after loading the view.
+    }
+
+
+}
+
